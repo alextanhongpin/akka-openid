4cc3f628c9ddf0f7af9d2c58fc94de4adacaf445
Hong Pin Alex Tan <alextan@administrators-MacBook-Pro.local>
2017-06-29 10:57:27 +0800

    Update router

--- src/main/scala/com/alextanhongpin/app/Main.scala
+++ src/main/scala/com/alextanhongpin/app/Main.scala
@@ -10,12 +10,28 @@ import scala.util.{Failure, Success}
 import com.alextanhongpin.service._
 
 // https://spindance.com/reactive-rest-services-akka-http/
+// FooRouter.scala
+
+// object FooRouter {
+//   val route = path("foo") {
+//       complete {
+//          Ok -> "foo"
+//       } 
+//   }       
+// }
+
+// object MainRouter {
+//    val routes = FooRouter.route ~ BarRouter.route
+// }
+
+
 object AkkaHttpHelloWorld extends HealthService {
     val host = "localhost"
     val port = 8080
+    val routes = route
 
     def main(args: Array[String]): Unit = {
-        val bindingFuture = Http().bindAndHandle(route, host, port)
+        val bindingFuture = Http().bindAndHandle(routes, host, port)
         println("listening to port *:8080. press RETURN to cancel")
         StdIn.readLine() // let it run until user presses return
 
